8d10cd143ccf703361eadf79e5ddf5a8533f6c59
Jonathan Leveque <jon.leveque@gmail.com>
2025-02-11 21:06:51 +0100

    fix bug

--- implicit_recommender.py
+++ implicit_recommender.py
@@ -112,36 +112,28 @@ recommender = RecommenderSystem(
 
 # 📌 Fonction Lambda
 def lambda_handler(event, context):
-    print(f"🚀 Exécution de la Lambda avec l'événement : {json.dumps(event)}")
+    print("🚀 Exécution de la Lambda...")
+    print(f"🚀 Événement reçu par Lambda : {json.dumps(event)}")
+    user_id = event.get("user_id")
+    if not user_id:
+        return {"statusCode": 400, "body": json.dumps({"error": "user_id is required"})}
 
     try:
-        # 🔹 Vérifier si API Gateway a envoyé un body JSON encodé
-        body = json.loads(event["body"])  # 🔥 Convertir le JSON string en dictionnaire
-        user_id = body.get("user_id")
-
-        if not user_id:
-            return {"statusCode": 400, "body": json.dumps({"error": "user_id is required"})}
-
-        user_id = int(user_id)  # 🔥 Convertir en entier
-
-        # ✅ Générer les recommandations ALS pour l'utilisateur
-        recommendations = recommender.recommend_articles(user_id)
-
-        # ✅ Vérifier si recommend_articles() a retourné une erreur
-        if isinstance(recommendations, dict):
-            return recommendations  # Retourne directement l'erreur si elle existe
-
-        # ✅ Stocker les recommandations dans DynamoDB
-        recommender.store_recommendations_in_dynamodb(user_id, recommendations)
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"user_id": user_id, "recommendations": recommendations})
-        }
-
-    except json.JSONDecodeError:
-        return {"statusCode": 400, "body": json.dumps({"error": "Invalid JSON format"})}
+        user_id = int(user_id)
     except ValueError:
         return {"statusCode": 400, "body": json.dumps({"error": "Invalid user_id format"})}
-    except Exception as e:
-        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
+
+    # ✅ Générer les recommandations ALS pour l'utilisateur
+    recommendations = recommender.recommend_articles(user_id)
+
+    # ✅ Vérifier si `recommend_articles()` a retourné une erreur
+    if isinstance(recommendations, dict):
+        return recommendations  # Retourne directement l'erreur si elle existe
+
+    # ✅ Stocker les recommandations dans DynamoDB
+    recommender.store_recommendations_in_dynamodb(user_id, recommendations)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"user_id": user_id, "recommendations": recommendations})
+    }
